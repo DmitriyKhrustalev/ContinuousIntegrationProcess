dba75f927ddd2377c9b3bae64ab625e15d4338b1
Net test <hryst2092@mail.ru>
2024-12-16 20:21:17 +0300

    add tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.statistic;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
 
@@ -11,6 +11,30 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxZeroValues() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 0, 0, 0, 0};
+        long expected = 0;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxNegativeValues() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-11, -6, -9, -1, -2};
+        long expected = -1;
 
         long actual = service.findMax(incomesInBillions);
 
